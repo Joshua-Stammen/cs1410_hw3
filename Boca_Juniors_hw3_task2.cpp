b695b2d6e49df3c100454364e248899b399772f8
Joshua_Stammen <joshuastammen@mail.weber.edu>
2017-09-26 12:59:52 -0600

    Added second task page

---
+++ Boca_Juniors_hw3_task2.cpp
@@ -0,0 +1,32 @@
+
+
+
+// File: Boca_Juniors_hw3_task2.cpp
+
+// Created by Joshua Stammen on 9/26/2017 for CS1410.
+
+
+// Copyright (c) 2017 WSU
+
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+// Constants,Structs, Classes
+
+
+// Prototypes
+
+
+// Main Program Program
+
+int main(void) {
+
+
+    return 0;
+
+}
+// Function Definitions
