7154100e803db4da1c2bb9a93f5ec0cc7c950b74
Joshua_Stammen <joshuastammen@mail.weber.edu>
2017-09-28 09:55:35 -0600

    Added get_date

--- Boca_Juniors_hw3_task1.cpp
+++ Boca_Juniors_hw3_task1.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
-int main() {
+ class Date
+{
+ private:
+     int month;
+     int day;
+     int year;
+ public:
+
+     void set_date(int mon, int d, int y)
+     {
+         month = mon;
+         day = d;
+         year = y;
+
+     }
+
+     void get_date()
+     {
+         cout << " \nEnter your first date format(12/31/99): ";
+         cin >> month,day, year;
+     }
+
+ };
+
+
+int main(void) {
+
+    Date d1,d2;
+
+
+
 
     return 0;
 }
